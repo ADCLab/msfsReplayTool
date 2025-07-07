f1e287e50a940b114f57ebf7d2e8af9ac9faa434
MSI <lopez212318@gmail.com>
2025-07-07 19:55:09 -0400

    connected to SimConnect API, takes in long and lat

---
+++ msfsReplayTool/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace msfsReplayTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelLongitude = new System.Windows.Forms.Label();
+            this.labelLatitude = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonConfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelLongitude
+            // 
+            this.labelLongitude.AutoSize = true;
+            this.labelLongitude.Location = new System.Drawing.Point(229, 174);
+            this.labelLongitude.Name = "labelLongitude";
+            this.labelLongitude.Size = new System.Drawing.Size(85, 20);
+            this.labelLongitude.TabIndex = 0;
+            this.labelLongitude.Text = "Longtitude";
+            this.labelLongitude.Click += new System.EventHandler(this.Longitude_Click);
+            // 
+            // labelLatitude
+            // 
+            this.labelLatitude.AutoSize = true;
+            this.labelLatitude.Location = new System.Drawing.Point(579, 174);
+            this.labelLatitude.Name = "labelLatitude";
+            this.labelLatitude.Size = new System.Drawing.Size(67, 20);
+            this.labelLatitude.TabIndex = 1;
+            this.labelLatitude.Text = "Latitude";
+            this.labelLatitude.Click += new System.EventHandler(this.Latitude_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 240);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(562, 240);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.Location = new System.Drawing.Point(399, 331);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(108, 67);
+            this.buttonConfirm.TabIndex = 4;
+            this.buttonConfirm.Text = "Confirm";
+            this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.Click += new System.EventHandler(this.buttonConfirm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(929, 505);
+            this.Controls.Add(this.buttonConfirm);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelLatitude);
+            this.Controls.Add(this.labelLongitude);
+            this.Name = "Form1";
+            this.Text = "Picture Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLongitude;
+        private System.Windows.Forms.Label labelLatitude;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonConfirm;
+    }
+}
+
---
+++ msfsReplayTool/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.FlightSimulator.SimConnect; // msfs yess
+using System.Runtime.InteropServices; // for SimConnect API
+
+namespace msfsReplayTool
+{
+    
+
+    public partial class Form1 : Form
+    {
+
+        SimConnect simconnect = null;
+        const int WM_USER_SIMCONNECT = 0x0402; // windows message for SimConnect
+
+       /* This is an actual struct from the api, but just to keep things simple
+        * im going to create my own just to get lat, lon, and alt.
+        struct SIMCONNECT_DATA_INITPOSITION
+        {
+            public double latitude; // Degrees
+            public double longitude; // Degrees
+            public double altitude; // Feet
+            public double pitch; // Degrees
+            public double bank; // Degrees
+            public double heading; // Degrees
+            public float airspeed; // Knots
+            public float verticalSpeed; // Feet per minute
+        }
+       */
+
+        struct aircraftPosition
+        {
+            public double latitude; // degrees
+            public double longitude; // degrees
+            public double altitude; // feet
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                simconnect = new SimConnect("connection", this.Handle, WM_USER_SIMCONNECT, null, 0); // establish connection to MSFS
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show("SimConnect connection failed: " + ex.Message, "there's been an error");
+            }
+        }
+
+        private void Longitude_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Latitude_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConfirm_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // convert input from textboxes to doubles 
+                double lat = double.Parse(labelLatitude.Text);
+                double lon = double.Parse(labelLongitude.Text);
+                double alt = 1500.0; // default altitude for now
+
+                // initialize aircraftPosition struct and give values
+                aircraftPosition position;
+                position.latitude = lat;
+                position.longitude = lon;
+                position.altitude = alt;
+
+                // AddToDataDefinition doesn't accept integer values
+                // this doesnt work fix tomorrow
+                Enum DefineID
+                {
+                    aircraftPosition = 0
+                }
+
+                // basically interprets data from the struct and sends it to MSFS
+                simconnect.AddToDataDefinition(DefineID.aircraftPosition, "PLANE LATITUDE", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DefineID.aircraftPosition, "PLANE LONGITUDE", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.AddToDataDefinition(DefineID.aircraftPosition, "PLANE ALTITUDE", "feet", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED);
+                simconnect.RegisterDataDefineStruct<aircraftPosition>(DefineID.aircraftPosition); // register the struct with SimConnect
+
+                simconnect.SetDataOnSimObject(DefineID.aircraftPosition, SimConnect.SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_DATA_SET_FLAG.DEFAULT, position); // send the data to MSFS
+
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    
+}
